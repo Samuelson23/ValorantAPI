bd8a91ec43543f711a50cb2ee0aaaf5ebff3816a
Samuelson23 <3samuelmg@gmail.com>
2023-08-07 22:42:00 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { NavLink, Navigate, useNavigate } from 'react-router-dom'
 
+import "./Header.css"
 const Header = () => {
+  const navigate=useNavigate()
   return (
-    <div>Header</div>
+    <header>
+      <div className="logoHome" onClick={()=> navigate("/")}>
+      <img 
+        src=""
+        
+      />
+      </div>
+      <NavLink to="/agents">AGENTS</NavLink>
+      <NavLink to="/maps">MAPS</NavLink>
+      <NavLink to="/weapons">WEAPONS</NavLink>
+      <NavLink to="/sprays">SPRAYS</NavLink>
+    </header>
   )
 }
 
