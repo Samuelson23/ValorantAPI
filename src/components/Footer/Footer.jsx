3af90ea483f2697bf6b2ed9bff54d580b44e3679
Samuelson23 <3samuelmg@gmail.com>
2023-08-08 11:03:21 +0200

    footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,17 @@ const Footer = () => {
   const navigate = useNavigate()
   return (
     <footer>
+      <a href="https://www.linkedin.com/in/samuel-martinez-gonzalez-640594266/" target="_blank" rel="noopener noreferrer">
       <img 
         src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481491/icons8-linkedin-48_phhzaj.png"
-        onClick={()=> navigate("https://www.linkedin.com/in/samuel-martinez-gonzalez-640594266/")}
       />
+      </a>
+      <p>Samuel Martinez ©</p>
+      <a href="https://github.com/Samuelson23/ValorantAPI" target="_blank" rel="noopener noreferrer">
       <img 
         src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481489/icons8-github-64_fbjunw.png"
-        onClick={()=> navigate("https://github.com/Samuelson23/ValorantAPI")}
       />
+      </a>
     </footer>
   )
 }
