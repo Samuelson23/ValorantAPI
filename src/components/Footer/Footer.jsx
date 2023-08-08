fd3dd9e96c542538de5b5460059c3e6034295b38
Samuelson23 <3samuelmg@gmail.com>
2023-08-08 10:00:25 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./Footer.css"
+import { useNavigate } from 'react-router-dom'
 
 const Footer = () => {
+  const navigate = useNavigate()
   return (
-    <div>Footer</div>
+    <footer>
+      <img 
+        src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481491/icons8-linkedin-48_phhzaj.png"
+        onClick={()=> navigate("https://www.linkedin.com/in/samuel-martinez-gonzalez-640594266/")}
+      />
+      <img 
+        src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481489/icons8-github-64_fbjunw.png"
+        onClick={()=> navigate("https://github.com/Samuelson23/ValorantAPI")}
+      />
+    </footer>
   )
 }
 
