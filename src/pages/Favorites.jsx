d8f0b9582e66cbf662b2612eeaf93d451da878ca
Samuelson23 <3samuelmg@gmail.com>
2023-08-09 14:17:57 +0200

    favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { favAgents } from '../data/dataFavorites'
+import { arrayAgents } from '../data/dataGlobal'
+import PrintFavsAgents from '../components/PrintFavs/PrintFavsAgents'
+import "./Favorites.css"
 
 const Favorites = () => {
+  console.log("favoritessss", favAgents)
+
   return (
-    <div>Favorites</div>
+    <>
+    <div className="divFavAgents">
+      <PrintFavsAgents data={favAgents}/>
+    </div>
+    <div className="divFavMaps">
+      
+    </div>
+    <div className="divFavWeapons">
+
+    </div>
+    <div className="divFavSprays">
+
+    </div>
+    </>
   )
 }
 
