051ef6a17ac6e71e78ad740533b6a07d3370a40e
Samuelson23 <3samuelmg@gmail.com>
2023-08-21 20:37:43 +0200

    favorite page with components and data

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { favAgents } from '../data/dataFavorites'
-import { arrayAgents } from '../data/dataGlobal'
+import { favAgents, favMaps, favSprays, favWeapons } from '../data/dataFavorites'
+import { arrayAgents, arrayMaps, arraySprays, arrayWeapons } from '../data/dataGlobal'
 import PrintFavsAgents from '../components/PrintFavs/PrintFavsAgents'
 import "./Favorites.css"
+import PrintFavsMaps from '../components/PrintFavs/PrintFavsMaps'
+import PrintFavsWeapons from '../components/PrintFavs/PrintFavsWeapons'
+import PrintFavsSprays from '../components/PrintFavs/PrintFavsSprays'
+
 
 const Favorites = () => {
   console.log("favoritessss", favAgents)
@@ -11,19 +15,19 @@ const Favorites = () => {
     <>
     <h2>AGENTS:</h2>
     <div className="divFavAgents">
-      <PrintFavsAgents data={favAgents}/>
+      <PrintFavsAgents data={favAgents} array={arrayAgents}/>
     </div>
     <h2>MAPS:</h2>
     <div className="divFavMaps">
-      
+      <PrintFavsMaps data={favMaps}/>
     </div>
     <h2>WEAPONS:</h2>
     <div className="divFavWeapons">
-
+      <PrintFavsWeapons data={favWeapons} array={arrayWeapons}/>
     </div>
     <h2>SPRAYS:</h2>
     <div className="divFavSprays">
-
+      <PrintFavsSprays data={favSprays}/>
     </div>
     </>
   )
