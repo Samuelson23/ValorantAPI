6161700c3c1a69b2df2ac1b156ac5268fb4ce868
Samuelson23 <3samuelmg@gmail.com>
2023-08-23 15:16:42 +0200

    home page with styled components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import H2 from '../components/ui/H2.element'
 import P from '../components/ui/P.element'
 import Button from '../components/ui/Button.element'
 import Section from '../components/ui/Section.element'
 import DivLayout from '../components/ui/DivLayout.element'
 import { useNavigate} from 'react-router-dom'
-
+import "./Home.css"
 const Home = () => {
     const navigate = useNavigate()
+    const [scr, setScr] = useState(0)
+    const controlScroll = () => {
+        const sections = document.querySelectorAll(".animatedSectionOff")
+        const scrollTop = document.documentElement.scrollTop
+
+        console.log("altura scroll",scrollTop)
+
+        sections.forEach((elem,index)=>{
+            let altura = sections[index].offsetTop
+            console.log("altura section",altura)
+            if(altura-550<scrollTop){
+                sections[index].style.opacity=1
+
+            }else{
+                sections[index].style.opacity=0;
+
+            }
+            
+        })
+    }
+
+    window.addEventListener("scroll", controlScroll)
+
   return (
     <>
     <DivLayout>
@@ -30,7 +53,7 @@ const Home = () => {
                 />
             </a>
         </div> */}
-        <Section>
+        <Section className={"animatedSection"}>
             <P text={"El shooter táctico 5v5 con personajes y habilidades"} fontSize={"big"} width={"75%"}></P>
             <H2 text={"VALORANT"}></H2>
             <P 
@@ -40,24 +63,42 @@ const Home = () => {
             
             padding={"10px 20px"}
             ></P>
-            <Button text={"Ver mas"}></Button> 
+            <div className="divHomeIntro">
+                <a href="https://www.linkedin.com/in/samuel-martinez-gonzalez-640594266/" target="_blank" rel="noopener noreferrer">
+                    <img 
+                    src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481491/icons8-linkedin-48_phhzaj.png"
+                    />
+                </a>
+                <P 
+                text={"Web designed by Samuel Martinez. All copyright its from Riot Games API"} 
+                fontSize={"small"}
+                bkColor={"var(--whiteColor)"}
+                color={"var(--blackColor)"}
+                width={"50vw"}
+                ></P>
+                <a href="https://github.com/Samuelson23/ValorantAPI" target="_blank" rel="noopener noreferrer">
+                    <img 
+                        src="https://res.cloudinary.com/dy25vd1yu/image/upload/v1691481489/icons8-github-64_fbjunw.png"
+                    />
+                </a>
+            </div>
         </Section>
-        <Section>
+        <Section className={"animatedSection"}>
             <P text={"Descubre toda la información sobre los diferentes agentes del juego"} fontSize={"big"} width={"75%"}></P>
             <H2 text={"AGENTS"}></H2>
             <Button text={"Ver mas"} action={()=> navigate("/agents")}></Button> 
         </Section>
-        <Section>
+        <Section className={"animatedSectionOff"}>
             <P text={"Echa un vistazo a los mapas para crear la mejor estrategia"} fontSize={"big"} width={"75%"}></P>
             <H2 text={"MAPS"}></H2>
             <Button text={"Ver mas"} action={()=> navigate("/maps")}></Button> 
         </Section>
-        <Section>
+        <Section className={"animatedSectionOff"}>
             <P text={"¿No sabes que arma elegir? Aquí tienes toda la información sobre ellas. Y también las diferentes skins"} fontSize={"big"} width={"75%"}></P>
             <H2 text={"WEAPONS"}></H2>
             <Button text={"Ver mas"} action={()=> navigate("/weapons")}></Button> 
         </Section>
-        <Section>
+        <Section className={"animatedSectionOff"}>
             <P text={"No te pierdas los sprays del juego, ya que puedes hacer graffittis mientras estas luchando"} fontSize={"big"} width={"75%"}></P>
             <H2 text={"SPRAYS"}></H2>
             <Button text={"Ver mas"} action={()=> navigate("/sprays")}></Button> 
