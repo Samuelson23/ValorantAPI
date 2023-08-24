e9b96befc017f6fc99267f4ddb4f299269999b46
Samuelson23 <3samuelmg@gmail.com>
2023-08-24 10:14:54 +0200

    refresh route

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home';
 
 function App() {
-
+  const res = new Response()
+  console.log("res",res)
   return (
     <>
-      <Header/>
-      <main><Outlet/></main>
-      <Footer/>
+      {
+        res.status==200
+        ? (
+          <>
+          <Header/>
+          <main><Outlet/></main>
+          <Footer/>
+          </>
+        )
+        : (
+          <>
+          <Header/>
+          <main><Home/></main>
+          <Footer/>
+          </>
+        )
+      }
     </>
   )
 }
