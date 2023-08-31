95953f3bad65698bc022bae2564c75dfb82cc63e
Samuelson23 <3samuelmg@gmail.com>
2023-08-31 09:35:12 +0200

    weapons section rework -> title

--- src/components/PrintCardWeapons/PrintCardWeapons.jsx
+++ src/components/PrintCardWeapons/PrintCardWeapons.jsx
@@ -18,99 +18,105 @@ Filtramos las armas por categorias para imprimirlas en diferentes divs y asi ten
 
   return (
     <div className="divCardsWeapons">
-        <h3>PISTOLS</h3>
-        <div className="divPistols">
-        {
-            pistols.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img 
-                            src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
-        {
-            melee.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
+        <div className="divGeneralWeapons">
+            <h2>PISTOLS</h2>
+            <div className="divPistols">
+            {
+                pistols.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img 
+                                src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            {
+                melee.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            </div>
         </div>
-        <h3>SMG, SHOTGUN AND HEAVY WEAPON</h3>
-        <div className="divSmgsShotHeavy">
-        {
-            smgs.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
-        {
-            shotguns.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
-        {
-            heavyWeapons.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
+        <div className="divGeneralWeapons">
+            <h2>SMG, SHOTGUN AND HEAVY WEAPON</h2>
+            <div className="divSmgsShotHeavy">
+            {
+                smgs.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            {
+                shotguns.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            {
+                heavyWeapons.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            </div>
         </div>
-        <h3>RIFLES</h3>
-        <div className="divRifles">
-        {
-            riffles.map((elem)=>{
-                return(
-                    <section key={elem.uuid}>
-                        <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
-                            <h2>{elem.displayName}</h2>
-                            <img src={elem.displayIcon} alt="weapon imagen" />
-                            
-                        </figure>
-                        <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
-                    </section>
-                )
-            })
-        }
+        <div className="divGeneralWeapons">
+            <h2>RIFLES</h2>
+            <div className="divRifles">
+            {
+                riffles.map((elem)=>{
+                    return(
+                        <section key={elem.uuid}>
+                            <figure className="figureCardWeapon" onClick={()=> navigate(`/weapons/${elem.displayName}`)}>
+                                <h3>{elem.displayName}</h3>
+                                <img src={elem.displayIcon} alt="weapon imagen" />
+                                
+                            </figure>
+                            <button onClick={()=> navigate(`/weapons/${elem.displayName}`)} className="btnInformation">MORE INFO</button>
+                        </section>
+                    )
+                })
+            }
+            </div>
         </div>
     </div>
   )
